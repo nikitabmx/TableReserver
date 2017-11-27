d335fea8601894c414d92b880e796d3e7d2ae7a1
zibellon <artem199800@mail.ru>
2017-11-27 20:19:20 +0300

    первая от меня 27 11

--- app/src/main/java/com/example/nekitenzogmailcom/tablereserver/FirstFloor.java
+++ app/src/main/java/com/example/nekitenzogmailcom/tablereserver/FirstFloor.java
@@ -10,9 +10,6 @@ import android.view.ViewGroup;
 
 
 /**
- *
- *
- *
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
  * {@link FirstFloor.OnFragmentInteractionListener} interface
