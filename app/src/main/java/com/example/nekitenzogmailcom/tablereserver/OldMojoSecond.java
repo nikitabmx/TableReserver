4e2b964612e47e653033ee00c0cffb08b57db608
nikitabmx <nekitenzo@gmail.com>
2017-11-27 19:09:31 +0300

    Артем сосет хер(первая заливка ему в рот)

---
+++ app/src/main/java/com/example/nekitenzogmailcom/tablereserver/OldMojoSecond.java
@@ -0,0 +1,97 @@
+package com.example.nekitenzogmailcom.tablereserver;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+public class OldMojoSecond extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    Context context;
+
+    private OnFragmentInteractionListener mListener;
+
+    public OldMojoSecond() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment OldMojoSecond.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static OldMojoSecond newInstance(String param1, String param2) {
+        OldMojoSecond fragment = new OldMojoSecond();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_old_mojo_second, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+
+        this.context = context;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+
+    public void onTestLayoutClick(View view) {
+        Toast.makeText(context,"kek ", Toast.LENGTH_LONG );
+    }
+}
---
+++ app/src/main/java/com/example/nekitenzogmailcom/tablereserver/card_activity.java
@@ -0,0 +1,13 @@
+package com.example.nekitenzogmailcom.tablereserver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class card_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_activity);
+    }
+}
